b0678169d9777cd2cea845e75950fb9dba5641ce
HyperBitGore <bruiserjoe01@gmail.com>
2023-03-22 10:27:09 -0500

    gameboard
    gameboard

--- ConnectFourGUI.java
+++ ConnectFourGUI.java
@@ -5,7 +5,6 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
-import net.miginfocom.swing.MigLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JSlider;
@@ -34,67 +33,81 @@ public class ConnectFourGUI extends JPanel {
 	public ConnectFourGUI() {
 		setBackground(Color.BLACK);
 		setForeground(new Color(0, 0, 0));
-		setLayout(new MigLayout("", "[31.00][113.00][301.00px]", "[20px][14px][][][][][][][][][][]"));
+		setLayout(null);
 		
 		lblNewLabel = new JLabel("CONNECT 4");
+		lblNewLabel.setBounds(0, 0, 0, 0);
 		lblNewLabel.setForeground(Color.GREEN);
 		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		add(lblNewLabel, "cell 2 0,alignx center,aligny top");
+		add(lblNewLabel);
 		
 		lblPlayer_1_Indicator = new JLabel(">");
+		lblPlayer_1_Indicator.setBounds(0, 0, 0, 0);
 		lblPlayer_1_Indicator.setForeground(Color.WHITE);
-		add(lblPlayer_1_Indicator, "flowx,cell 2 10");
+		add(lblPlayer_1_Indicator);
 		
 		lbl_Player1 = new JLabel("PLAYER 1");
+		lbl_Player1.setBounds(0, 0, 0, 0);
 		lbl_Player1.setVerticalAlignment(SwingConstants.BOTTOM);
 		lbl_Player1.setForeground(Color.GREEN);
-		add(lbl_Player1, "cell 2 10,growx,aligny top");
+		add(lbl_Player1);
 		
 		lblColumn0 = new JLabel("___");
+		lblColumn0.setBounds(0, 0, 0, 0);
 		lblColumn0.setForeground(Color.WHITE);
-		add(lblColumn0, "flowx,cell 2 8");
+		add(lblColumn0);
 		
 		lblColumn1 = new JLabel("___");
+		lblColumn1.setBounds(0, 0, 0, 0);
 		lblColumn1.setForeground(Color.WHITE);
-		add(lblColumn1, "cell 2 8");
+		add(lblColumn1);
 		
 		lblColumn2 = new JLabel("___");
+		lblColumn2.setBounds(0, 0, 0, 0);
 		lblColumn2.setForeground(Color.WHITE);
 		lblColumn2.setBackground(Color.WHITE);
-		add(lblColumn2, "cell 2 8");
+		add(lblColumn2);
 		
 		lblColumn3 = new JLabel("___");
+		lblColumn3.setBounds(0, 0, 0, 0);
 		lblColumn3.setForeground(Color.WHITE);
-		add(lblColumn3, "cell 2 8");
+		add(lblColumn3);
 		
 		lblColumn4 = new JLabel("___");
+		lblColumn4.setBounds(0, 0, 0, 0);
 		lblColumn4.setForeground(Color.WHITE);
-		add(lblColumn4, "cell 2 8");
+		add(lblColumn4);
 		
 		lblColumn5 = new JLabel("___");
+		lblColumn5.setBounds(0, 0, 0, 0);
 		lblColumn5.setForeground(Color.WHITE);
-		add(lblColumn5, "cell 2 8");
+		add(lblColumn5);
 		
 		lblColumn6 = new JLabel("___");
+		lblColumn6.setBounds(0, 0, 0, 0);
 		lblColumn6.setForeground(Color.WHITE);
-		add(lblColumn6, "cell 2 8");
+		add(lblColumn6);
 		
 		btnPause = new JButton("PAUSE");
+		btnPause.setBounds(0, 0, 0, 0);
 		btnPause.setBackground(Color.DARK_GRAY);
 		btnPause.setForeground(Color.BLACK);
 		btnPause.setFont(new Font("Times New Roman", Font.PLAIN, 11));
-		add(btnPause, "cell 2 10");
+		add(btnPause);
 		
 		lblPlayer_2_Indicator = new JLabel(">");
+		lblPlayer_2_Indicator.setBounds(0, 0, 0, 0);
 		lblPlayer_2_Indicator.setForeground(Color.WHITE);
-		add(lblPlayer_2_Indicator, "flowx,cell 2 11");
+		add(lblPlayer_2_Indicator);
 		
 		lbl_Player2 = new JLabel("PLAYER 2");
+		lbl_Player2.setBounds(0, 0, 0, 0);
 		lbl_Player2.setVerticalAlignment(SwingConstants.BOTTOM);
 		lbl_Player2.setForeground(Color.GREEN);
-		add(lbl_Player2, "cell 2 11,growx,aligny top");
+		add(lbl_Player2);
 		
 		btnExit = new JButton("EXIT");
+		btnExit.setBounds(0, 0, 0, 0);
 		btnExit.setBackground(Color.DARK_GRAY);
 		btnExit.setForeground(Color.BLACK);
 		btnExit.setFont(new Font("Times New Roman", Font.PLAIN, 11));
@@ -102,7 +115,7 @@ public class ConnectFourGUI extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		add(btnExit, "cell 2 11");
+		add(btnExit);
 
 		
 		
--- Gameboard.java
+++ Gameboard.java
@@ -1,29 +1,65 @@
 import java.util.ArrayList;
 
 public class Gameboard {
-	ArrayList<Piece> pieces;
-	boolean[][] board = new boolean[7][6];
+	private ArrayList<Piece> pieces;
+	private int[][] board = new int[7][6];
+	private int lastColumn = -1;
+	private int lastRow = -1;
 	public Gameboard() {
+		pieces = new ArrayList<>();
 		for(int i = 0; i < 7; i++) {
 			for(int j = 0; j < 6; j++) {
-				board[i][j] = false;
+				board[i][j] = 0;
 			}
 		}
 	}
+	//columns go from 0-6
 	boolean createPiece(int column) {
 		//calculate end position and return false if column is filled
 		int end = 0;
 		for(int i = 0; i < 6; i++, end++) {
-			if(board[column][i]) {
+			if(board[column][i] == 1) {
 				i = 6;
 			}
 		}
+		end--;
 		//fill board cell it ends on
-		board[column][end] = true;
+		board[column][end] = 1;
+		lastRow = end;
+		lastColumn = column;
 		//add piece to pieces
 		Piece p = new Piece();
 		pieces.add(p);
 		return true;
+	}
+	//returns last move made
+	public int getLastColumn() {
+		return lastColumn;
+	}
+	public int getLastRow() {
+		return lastRow;
+	}
+	public ArrayList<Piece> getPieces() {
+		return pieces;
+	}
+	public void setPieces(ArrayList<Piece> pieces) {
+		this.pieces = pieces;
+	}
+	public int[][] getBoard() {
+		return board;
+	}
+	public static void main(String[] args) {
+		Gameboard g = new Gameboard();
+		g.createPiece(0);
+		g.createPiece(0);
+		g.createPiece(0);
+		int[][] b = g.getBoard();
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 6; j++) {
+				System.out.print(b[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 }
