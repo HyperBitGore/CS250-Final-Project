9a0b94d7c45c108974cab19c3238ea9cd5710dd8
zl9055li <116689841+zl9055li@users.noreply.github.com>
2023-03-22 11:34:05 -0500

    Create AI.java

---
+++ AI.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class AI {
+	
+	public int turn() {
+		
+		Gameboard gameboard = new Gameboard();
+		gameboard.getBoard();		//Get the whole board
+		
+		if(Player.isTurn(true))
+		
+		return 0;
+		
+	}
+}
